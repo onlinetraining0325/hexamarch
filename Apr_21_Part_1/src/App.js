66be200910bfa02791c9e9d63b6fe9b215c86a4e
b69user39@rpsconsulting.in <prasanna.trainer@gmail.com>
2026-04-21 09:21:14 +0530

    code

---
+++ Apr_21_Part_1/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './components/first/first';
+import Second from './components/second/second';
+import Third from './components/third/third';
+import Four from './components/four/four';
+import Five from './components/five/five';
+import Six from './components/six/six';
+import Seven from './components/seven/seven';
+import Eight from './components/eight/eight';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Welcome to React from Hexaware</p>
+     <First /> 
+     <Second />
+     <Third firstName="Sam" lastName="Arlin" company="hexaware" />
+     <Four /> 
+     <Five />
+     <Six />
+     <Seven />
+     <Eight />
+    </div>
+  );
+}
+
+export default App;
---
+++ Apr_21_Part_1/src/components/five/five.js
@@ -0,0 +1,26 @@
+import React, {Component, useState} from 'react';
+
+const Five = () => {
+
+  const [count,setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count+1);
+  }
+
+  const decrement = () => {
+    setCount(count-1);
+  }
+
+  return(
+    <div>
+      <p>Count is : <b>{count}</b></p>
+      <br/>
+      <input type="button" value="Increment" onClick={increment} />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <input type="button" value="Decrement" onClick={decrement}  />
+    </div>
+  )
+}
+
+export default Five;
