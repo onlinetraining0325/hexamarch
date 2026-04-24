ad09cbb0dd3632d507b94852ad0c51b584081db6
b69user39@rpsconsulting.in <prasanna.trainer@gmail.com>
2026-04-24 09:45:33 +0530

    code

---
+++ Apr_24_Jwt_P1/JWT_Script.sql
@@ -0,0 +1,16 @@
+create database jwtexample;
+
+use jwtexample;
+
+
+SELECT * FROM jwtexample.userdata;
+
+Create Table Employ
+(
+   eid int auto_increment primary key,
+   name varchar(30),
+   gender varchar(30),
+   email varchar(30),
+   password varchar(255),
+   roles varchar(100)
+);
